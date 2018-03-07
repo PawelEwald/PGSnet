d6572da05270a5a098f55d5f70297faedb9f0397
PawelEwald <pawel.ewald@gmail.com>
2018-03-07 23:33:20 +0100

    Zajęcia nr 1

---
+++ Pgs.Kanban.Api/Controllers/RandomGeneratorController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pgs.Kanban.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pgs.Kanban.Api.Controllers
+{
+    [Route("api/[controller]")]  //adnotacja - odcina slowo controller zostaje : http://localhost:54820/api/RandomGeneration/
+    public class RandomGeneratorController : Controller
+    {
+        private RandomGeneratorService randomGeneratorService;
+        public RandomGeneratorController()
+        {
+            randomGeneratorService = new RandomGeneratorService();
+        }
+
+        [HttpGet]
+        public IActionResult GetRandomNumber()
+        {
+            var number = randomGeneratorService.GenerateRandomNumber();
+            return Ok(number);
+        }
+        [HttpGet]
+        [Route("{maxValue}")]
+        public IActionResult GetRandomNumberInRange(int maxValue)
+        {
+
+            var number = randomGeneratorService.GenerateRandomNumber(maxValue);
+            return Ok(number);
+        }
+
+        [HttpPost]
+        public IActionResult AddNumber([FromBody]int number)
+        {
+            randomGeneratorService.AddNumberToList(number);
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteNumber(int number)
+        {
+            randomGeneratorService.DeleteNumber(number);
+            return NoContent();
+        }
+        [HttpGet]
+        [Route("AllNumbers")]
+        public IActionResult GetAllNumbers()
+        {
+            var numbers = randomGeneratorService.GetNumberList();
+            return Ok(numbers);
+        }
+    }
+}
---
+++ Pgs.Kanban.Domain/Services/RandomGeneratorService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pgs.Kanban.Domain.Services
+{
+    public class RandomGeneratorService
+    {
+
+        private static List<int> magicNumber = new List<int>();
+
+
+        private Random randomGenerator = new Random();
+        public int GenerateRandomNumber()
+        {
+
+
+            var number = randomGenerator.Next();
+            return number;
+
+        }
+        public int GenerateRandomNumber(int maxValue)
+        {
+
+            var number = randomGenerator.Next(maxValue);
+            return number;
+        }
+
+        public void AddNumberToList(int number)
+        {
+            magicNumber.Add(number);
+        }
+
+        public void DeleteNumber(int number)
+        {
+            magicNumber.Remove(number);
+        }
+        public List<int> GetNumberList()
+        {
+            return magicNumber;
+        }
+
+    }
+
+}
+
+
+
